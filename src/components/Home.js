cb01bc5e707d11894121276dd5fc87c433f0b4ae
VijayClaudius <kummarivijay1@gmail.com>
2024-03-01 21:28:58 -0600

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
       <h1>Welcome to Our Study Abroad Platform</h1>
       <p>
         Find the Best Accommodations, Transportation, and Employment
-        Opportunities for International Students.
+        Opportunities for International Students rajesh.
       </p>
       <a href="/Login">Login</a> or <a href="/signup">signup</a> or
       <button>Logout</button>
